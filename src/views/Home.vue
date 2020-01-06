e414eb1fe54f3647df0b4c1eeb5d72962aebac3a
WCY <wangcyyidou@163.com>
2020-01-06 16:17:56 +0800

    首页完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,271 @@
 <template>
     <div>
-        这是首页
+        <div class="box">
+            <div class="top">
+                <img src="@/assets/images/home_left.jpg">
+                <div class="tab">
+                    <a-tabs defaultActiveKey="1">
+                        <a-tab-pane tab="新闻发布" key="1">
+                            <ul>
+                                <li>
+                                    <span class="spot"></span>
+                                    <span class="title">
+                                        东城区持续推进老旧柴油货车淘汰更新，累计检查和处罚量均超上年全年水平
+                                    </span>
+                                    <span class="time">
+                                        2019-12-08
+                                    </span>
+                                </li>
+                                <li>
+                                    <span class="spot"></span>
+                                    <span class="title">
+                                        北京市生态环境局关于实施 国六机动车排放标准有关事项的通知
+                                    </span>
+                                    <span class="time">
+                                        2019-12-08
+                                    </span>
+                                </li>
+                                <li>
+                                    <span class="spot"></span>
+                                    <span class="title">
+                                        关于《关于实施国六机动车排放标准有关事项的通知》的解读
+                                    </span>
+                                    <span class="time">
+                                        2019-12-08
+                                    </span>
+                                </li>
+                                <li>
+                                    <span class="spot"></span>
+                                    <span class="title">
+                                        关于拟对首都儿科研究所附属儿童医院涉嫌超标排放水污染物进行重大处罚一案
+                                    </span>
+                                    <span class="time">
+                                        2019-12-08
+                                    </span>
+                                </li>
+                                <li>
+                                    <span class="spot"></span>
+                                    <span class="title">
+                                        北京市生态环境局通报10月16日-31日乡镇（街道）大气粗颗粒物浓度排名
+                                    </span>
+                                    <span class="time">
+                                        2019-12-08
+                                    </span>
+                                </li>
+                                <li>
+                                    <span class="spot"></span>
+                                    <span class="title">
+                                        北京市生态环境局关于对北京金隅红树林环保技术有限责任公司危险废物跨省转
+                                    </span>
+                                    <span class="time">
+                                        2019-12-08
+                                    </span>
+                                </li>
+                            </ul>
+                        </a-tab-pane>
+                        <a-tab-pane tab="通知公告" key="2" forceRender>
+                            <ul>
+                                <li>
+                                    <span class="spot"></span>
+                                    <span class="title">
+                                        通知公告
+                                    </span>
+                                    <span class="time">
+                                        2019-12-08
+                                    </span>
+                                </li>
+                                <li>
+                                    <span class="spot"></span>
+                                    <span class="title">
+                                        北京市生态环境局关于实施 国六机动车排放标准有关事项的通知
+                                    </span>
+                                    <span class="time">
+                                        2019-12-08
+                                    </span>
+                                </li>
+                                <li>
+                                    <span class="spot"></span>
+                                    <span class="title">
+                                        关于《关于实施国六机动车排放标准有关事项的通知》的解读
+                                    </span>
+                                    <span class="time">
+                                        2019-12-08
+                                    </span>
+                                </li>
+                                <li>
+                                    <span class="spot"></span>
+                                    <span class="title">
+                                        关于拟对首都儿科研究所附属儿童医院涉嫌超标排放水污染物进行重大处罚一案
+                                    </span>
+                                    <span class="time">
+                                        2019-12-08
+                                    </span>
+                                </li>
+                                <li>
+                                    <span class="spot"></span>
+                                    <span class="title">
+                                        北京市生态环境局通报10月16日-31日乡镇（街道）大气粗颗粒物浓度排名
+                                    </span>
+                                    <span class="time">
+                                        2019-12-08
+                                    </span>
+                                </li>
+                                <li>
+                                    <span class="spot"></span>
+                                    <span class="title">
+                                        北京市生态环境局关于对北京金隅红树林环保技术有限责任公司危险废物跨省转
+                                    </span>
+                                    <span class="time">
+                                        2019-12-08
+                                    </span>
+                                </li>
+                            </ul>
+                        </a-tab-pane>
+                        <a-tab-pane tab="国务院信息" key="3">
+                            <ul>
+                                <li>
+                                    <span class="spot"></span>
+                                    <span class="title">
+                                        国务院信息
+                                    </span>
+                                    <span class="time">
+                                        2019-12-08
+                                    </span>
+                                </li>
+                                <li>
+                                    <span class="spot"></span>
+                                    <span class="title">
+                                        北京市生态环境局关于实施 国六机动车排放标准有关事项的通知
+                                    </span>
+                                    <span class="time">
+                                        2019-12-08
+                                    </span>
+                                </li>
+                                <li>
+                                    <span class="spot"></span>
+                                    <span class="title">
+                                        关于《关于实施国六机动车排放标准有关事项的通知》的解读
+                                    </span>
+                                    <span class="time">
+                                        2019-12-08
+                                    </span>
+                                </li>
+                                <li>
+                                    <span class="spot"></span>
+                                    <span class="title">
+                                        关于拟对首都儿科研究所附属儿童医院涉嫌超标排放水污染物进行重大处罚一案
+                                    </span>
+                                    <span class="time">
+                                        2019-12-08
+                                    </span>
+                                </li>
+                                <li>
+                                    <span class="spot"></span>
+                                    <span class="title">
+                                        北京市生态环境局通报10月16日-31日乡镇（街道）大气粗颗粒物浓度排名
+                                    </span>
+                                    <span class="time">
+                                        2019-12-08
+                                    </span>
+                                </li>
+                                <li>
+                                    <span class="spot"></span>
+                                    <span class="title">
+                                        北京市生态环境局关于对北京金隅红树林环保技术有限责任公司危险废物跨省转
+                                    </span>
+                                    <span class="time">
+                                        2019-12-08
+                                    </span>
+                                </li>
+                            </ul>
+                        </a-tab-pane>
+                    </a-tabs>
+                </div>
+            </div>
+            <div class="bottom">
+                <img src="@/assets/images/home_footer.jpg">
+            </div>
+        </div>
     </div>
 </template>
+<style lang="scss" scoped>
+    .box {
+        width: 100%;
+        height: 100%;
+        padding: 40px;
+        .top {
+            overflow: hidden;
+            margin-bottom: 20px;
+            height: 349px;
+            img {
+                float: left;
+                margin-right: 20px;
+            }
+            /deep/
+            .tab {
+                width: calc(100% - 399px);
+                height: 100%;;
+                float: left;
+                ul {
+                    li {
+                        position: relative;
+                        height: calc(298px / 6);
+                        line-height: 49px;
+                        padding: 10px;
+                        &:nth-child(2n) {
+                            background-color: #f0f0f0;
+                        }
+                        span {
+                            position: absolute;
+                            top: 50%;
+                            transform: translateY(-50%);
+                            color: #333;
+                        }
+                        .spot {
+                            display: inline-block;
+                            width: 5px;
+                            height: 5px;
+                            border-radius: 50%;
+                            background-color: #333;
+                        }
+                        .title {
+                            display: inline-block;
+                            width: 80%;
+                            left: 25px;
+                            font-size: 18px;
+                            white-space: nowrap;
+                            overflow: hidden;
+                            text-overflow: ellipsis;
+                        }
+                        .time {
+                            right: 0;
+                            font-size: 18px;
+                            display: inline-block;
+                            width: 15%;
+                        }
+                    }
+                }
+                .ant-tabs-nav-container {
+                    line-height: 1;
+                }
+                .ant-tabs-nav .ant-tabs-tab {
+                    color: #333;
+                    font-weight: 700;
+                    font-size: 24px;
+                    padding: 0 16px 12px;
+                }
+                .ant-tabs-nav .ant-tabs-tab-active {
+                    color: #bd1a2d;
+                    font-weight: 700;
+                    font-size: 24px;
+                }
+                .ant-tabs-ink-bar {
+                    background-color: #bd1a2d;
+                }
+            }
+        }
+        .bottom {
+            height: calc(100% - 300px)
+        }
+    }
+</style>
